f032b80927752e2b3b984d24eef4ff347ea9a44d
idealmassagegit <51098522+idealmassagegit@users.noreply.github.com>
2020-01-13 21:53:09 -0600

    staff and services

--- js/functions.js
+++ js/functions.js
@@ -105,7 +105,40 @@ $(function(){
 	});
 
 
+	// Staff
 
+	$('.michael-image').click(function(){
+		if (!$(this).hasClass('active')) {
+			$('.pic-row img, .text-row div').removeClass('active');
+			$(this).addClass('active');
+			$('.michael').addClass('active');
+		}
+	});
+	$('.violet-image').click(function(){
+		if (!$(this).hasClass('active')) {
+			$('.pic-row img, .text-row div').removeClass('active');
+			$(this).addClass('active');
+			$('.violet').addClass('active');
+		}
+	});
+	$('.chelsey-image').click(function(){
+		if (!$(this).hasClass('active')) {
+			$('.pic-row img, .text-row div').removeClass('active');
+			$(this).addClass('active');
+			$('.chelsey').addClass('active');
+		}
+	});
+
+	$('.lang').click(function(){
+		if($(this).hasClass('esp')) {
+			$(this).text('Bilingual Booking Option!');
+			$(this).removeClass('esp').addClass('eng');
+			$('.lang-txt').text("Con su experiencia en el servicio de masajes, Michael se dedica especialmente a la prestación de masajes para que sus clientes logren una sensación de bienestar tanto física como mental, que les permita crear un equilibrio vital, desarrollar un mejor desempeño laboral y enfrentar de mejor manera el quehacer diario. Mike esta dedicado a ser un curador que usa su intuición y atención para ayudar a lograr sus metas.");
+		} else {
+			$(this).text('Se Habla Español!').removeClass('eng').addClass('esp');
+			$('.lang-txt').text("The focus of my massage practice is on therapeutic bodywork with a highly relaxing quality within a holistic, mind-body-spirit perspective. My healing work varies greatly, intuitively drawing upon many different healing traditions to meet the specific conditions that arise. My practice centers around listening to your body's messages and you, because you are the expert of your own body. Although I am a total massage geek, I still feel that caring, listening, intuition and tuning into energy are my best tools as a healer. I look forward to getting to know you and the miraculous ecosystem of your unique body.")
+		}
+	});
 
 
 
